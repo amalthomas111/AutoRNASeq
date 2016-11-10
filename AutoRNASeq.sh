726da72a0a21c37512fbcab0ed4362e813d48d5a
guilhermesena <guilhermesb14@gmail.com>
2016-11-09 22:23:36 -0800

    MakeCoutTable

--- AutoRNASeq.sh
+++ AutoRNASeq.sh
@@ -324,3 +324,30 @@ AutoRNASeqAll ()
     return 0
 }
 
+MakeCountTable ()  { 
+    numdirectories=`ls -d1 */ | wc -l | cut -f1`;
+    numvectors=`ls -1 */output*/*.vector | wc -l | cut -f1`;
+
+    echo "Num directories : $numdirectories";
+    echo "Num vectors : $numvectors";
+
+    # Checks if all output subdirectories have a *.vector file
+    if [ $numdirectories -ne $numvectors ]; then
+        echo "[ERROR] not all directories have count vectors";
+        return 1;
+    fi;
+
+    # Creates row names based on directory titles
+    printf "\t`ls -d1 */ | cut -f1 -d '/' | tr '\n' '\t'`\n" > _rownames.txt;
+
+    # Merges vectors
+    paste $PANASAS/ref_genomes/$1/${1}.genenames */output*/*.vector > count-table-nonames.txt;
+
+    # Concatenates row names and vectors
+    cat _rownames.txt count-table-nonames.txt > count-table-`basename $PWD`.txt;
+
+    # Remove temporary files
+    rm _rownames.txt count-table-nonames.txt;
+    return 0
+}
+
